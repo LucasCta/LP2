57bd19943b1646246f848924c4b4456176f8fc2e
LucasCta <lucas.ct.araujo@gmail.com>
2022-02-20 21:55:06 -0300

    Uploading Original RectApp.java

---
+++ 02-Java2D-Hello/RectApp/RectApp.java
@@ -0,0 +1,20 @@
+public class RectApp {
+    public static void main (String[] args) {
+        Rect r1 = new Rect(1,1, 10,10);
+        r1.print();
+    }
+}
+class Rect {
+    int x, y;
+    int w, h;
+    Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+}
