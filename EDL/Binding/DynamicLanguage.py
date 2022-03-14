7347e5dcf5003837932728b3f0353bb15ccca87b
LucasCta <lucas.ct.araujo@gmail.com>
2022-03-13 23:45:44 -0300

    Final DynamicLangauge.py and minor changes to StaticLanguage.c

---
+++ EDL/Binding/DynamicLanguage.py
@@ -0,0 +1,16 @@
+import math
+
+def f(a):
+    return math.sin(a)/10
+#nome = sin, propriedade = implementacao, tempo = ligacao
+
+print(10/10)
+#nome = 1, propriedade = valor, tempo = compilacao
+
+a = [30,40,50]
+#nome = a, propriedade = endereco, tempo = execucao
+#nome = a, propriedade = tipo, tempo = execucao
+
+for i in range(0,len(a)):
+    print(f(a))
+    #nome = print, propriedade = definicao, tempo = implementacao
--- EDL/Binding/StaticLanguage.c
+++ EDL/Binding/StaticLanguage.c
@@ -8,19 +8,19 @@ double pitagoras(double, double);
 //nome = pitagoras, propriedade = tipo de retorno, tempo = compilacao
 
 static double million = 1000000.0;
-//nome = million, tipo = endereco, tempo = carregamento
-//nome = double, tipo = tamanho, tempo = implementacao
+//nome = million, propriedade  = endereco, tempo = carregamento
+//nome = double, propriedade = tamanho, tempo = implementacao
 
 int main (void) {
 
     double golden_ratio = 1.6180339887498948482045868343656381177203091798057;
-    //nome = golden_ratio, tipo = endereco, tempo = execucao
+    //nome = golden_ratio, propriedade  = endereco, tempo = execucao
 
     int val = (int)(cos(pitagoras(golden_ratio, euler)) * million);
-    //nome = cos, tipo = implementacao, tempo = ligacao
-    //nome = val, tipo = endereco, tempo = execucao
-    //nome = *, tipo = semantica, tempo = compilacao
-    //nome = int, tipo tamanho, tempo = implementacao
+    //nome = cos, propriedade = implementacao, tempo = ligacao
+    //nome = val, propriedade = tipo, tempo = compilacao
+    //nome = *, propriedade  = semantica, tempo = compilacao
+    //nome = int, propriedade  tamanho, tempo = implementacao
 
     return val; 
 
