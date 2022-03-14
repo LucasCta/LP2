ad692c6cf0e524cb4779da8b914f342eeb749690
LucasCta <lucas.ct.araujo@gmail.com>
2022-03-13 21:54:03 -0300

    Updated Figures.c

--- 03-Packages/Figures.c
+++ 03-Packages/Figures.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+///////////////////////////////////////////////////////////////////////////////
 
 typedef struct {
     int r,g,b;
 } Color;
+
+Color* color_new (int x, int y, int z) {
+    Color* this = malloc(sizeof(Color));
+    this->r = x;
+    this->g = y;
+    this->b = z;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 struct Figure;
 typedef void (* Figure_Print) (struct Figure*);
@@ -19,20 +31,23 @@ typedef struct Figure {
 typedef struct {
     Figure super;
     int w, h;
-} Rect;
+} Ellipse;
 
-void rect_print (Rect* this) {
+void Ellipse_print (Ellipse* this) {
     Figure* sup = (Figure*) this;
-    printf("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+    printf("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
            this->w, this->h, sup->x, sup->y);
+    printf("Cores: (%d,%d,%d), (%d,%d,%d)\n\n", sup->fg.r, sup->fg.g, sup->fg.b, sup->bg.r, sup->bg.g, sup->bg.b); 
 }
 
-Rect* rect_new (int x, int y, int w, int h) {
-    Rect*   this  = malloc(sizeof(Rect));
+Ellipse* ellipse_new (int x, int y, int w, int h, Color lineColor, Color bgColor) {
+    Ellipse* this = malloc(sizeof(Ellipse));
     Figure* sup = (Figure*) this;
-    sup->print = (Figure_Print) rect_print;
+    sup->print = (Figure_Print) Ellipse_print;
     sup->x = x;
     sup->y = y;
+    sup->fg = lineColor;
+    sup->bg = bgColor;
     this->w = w;
     this->h = h;
 }
@@ -41,44 +56,51 @@ Rect* rect_new (int x, int y, int w, int h) {
 
 typedef struct {
     Figure super;
-    int w, h;
-} Ellipse;
+    int r, a, vx[6], vy[6];
+} Rhexagon;
 
-void Ellipse_print (Ellipse* this) {
+void Rhexagon_print (Rhexagon* this) {
     Figure* sup = (Figure*) this;
-    printf("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
-           this->w, this->h, sup->x, sup->y);
+    printf("Hexagono Regular na posicao (%d,%d), de raio %d, e rotacao de %d graus.\nVertices: ", 
+        sup->x, sup->y, this->r, this->a);
+    for (int i = 0; i < 6; i++){
+        printf("(%d,%d)", this->vx[i], this-> vy[i]);
+    }
+    printf("\nCores: (%d,%d,%d), (%d,%d,%d)\n\n", sup->fg.r, sup->fg.g, sup->fg.b, sup->bg.r, sup->bg.g, sup->bg.b); 
 }
 
-Ellipse* ellipse_new (int x, int y, int w, int h) {
-    Ellipse* this = malloc(sizeof(Ellipse));
+Rhexagon* rhexagon_new (int x, int y, int r, int a, Color lineColor, Color bgColor) {
+    Rhexagon* this = malloc(sizeof(Rhexagon));
     Figure* sup = (Figure*) this;
-    sup->print = (Figure_Print) Ellipse_print;
+    sup->print = (Figure_Print) Rhexagon_print;
     sup->x = x;
     sup->y = y;
-    this->w = w;
-    this->h = h;
+    sup->fg = lineColor;
+    sup->bg = bgColor;
+    this->r = r;
+    this->a = a;
+    for (int i = 0; i < 6; i++){
+        this->vx[i] = (int)(r*cos((a+i*60)*180/M_PI)) + x;
+        this->vy[i] = (int)(r*sin((a+i*60)*180/M_PI)) + y;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void main (void) {
-    Figure* figs[4] = {
-        (Figure*) rect_new(10,10,100,100),
-        (Figure*) ellipse_new(40,10,140,300),
-        (Figure*) rect_new(10,10,100,100),
-        (Figure*) ellipse_new(210,110,305,130)
+    Color* colors[3] = {
+        color_new(25,25,25),
+        color_new(255,255,255),
+        color_new(100,0,100)
     };
-
-    ///
-
+    Figure* figs[4] = {
+        (Figure*) ellipse_new(40,10,140,300,*colors[1],*colors[0]),
+        (Figure*) ellipse_new(210,110,305,130,*colors[2],*colors[1]),
+        (Figure*) rhexagon_new(120,80,100,15,*colors[1],*colors[2]),
+        (Figure*) rhexagon_new(170,40,60,0,*colors[0],*colors[2])
+    };
     for (int i=0; i<4; i++) {
         figs[i]->print(figs[i]);
-    }
-
-    ///
-
-    for (int i=0; i<4; i++) {
         free(figs[i]);
     }
 }
